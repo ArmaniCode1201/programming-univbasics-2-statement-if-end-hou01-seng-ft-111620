28d00d7db76980e423c3796c249d71a5eb8ff113
Misel Evtimov <misel.evtimov@gmail.com>
2020-11-01 21:09:41 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,6 @@
 # Write your solution here
-current_time = Time.now
-current_time = current_time.to_i
-if current_time % 2 == 0
+Time.now = Time.now.to_i
+if Time.now % 2 == 0
   puts "Even!"
 end
  puts "Odd!"
